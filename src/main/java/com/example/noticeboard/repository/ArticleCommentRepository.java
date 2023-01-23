9be885172f969467b33266a29f486124f4e426c0
youhobin <ghdb132@naver.com>
2023-01-23 10:38:21 +0900

    feat(ArticleCommentRepository) : 레포지토리 구현

---
+++ src/main/java/com/example/noticeboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,27 @@
+package com.example.noticeboard.repository;
+
+import com.example.noticeboard.domain.Article;
+import com.example.noticeboard.domain.ArticleComment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class ArticleCommentRepository {
+
+    private final EntityManager em;
+
+    public Long save(ArticleComment articleComment) {
+        em.persist(articleComment);
+        return articleComment.getId();
+    }
+
+    public List<ArticleComment> findAll() {
+        return em.createQuery("select ac from ArticleComment ac", ArticleComment.class)
+            .getResultList();
+    }
+
+}
